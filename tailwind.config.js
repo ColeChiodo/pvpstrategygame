ffe7397cd50b37e402041d82e656e81569643189
ColeChiodo <colechiodo@gmail.com>
2025-02-16 15:07:54 -0800

    Changed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,29 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        pixel: ['"VT323"', 'monospace'],
+        pixel: ['"Rubik"', 'serif'],
       },
       colors: {
         'primary': '#222034',
         'secondary': '#45283c',
       },
       fontSize: {
-        xs: '15px',    // Set the font size for xs to 10px
-        sm: '20px',    // Set the font size for sm to 20px
-        base: '30px',  // Default base font size (you can leave it or change)
-        md: '30px',
-        lg: '40px',    // Set the font size for lg to 24px
-        xl: '50px',    // Set the font size for xl to 32px
-        '2xl': '60px', // Set the font size for 2xl to 40px
-        '3xl': '70px', // Set the font size for 3xl to 48px
-        '4xl': '80px', // Set the font size for 4xl to 56px
-        '5xl': '90px', // Set the font size for 5xl to 64px
-        '6xl': '100px', // Set the font size for 6xl to 72px
-        '7xl': '110px', // Set the font size for 7xl to 80px
+        xs: '10px',
+        sm: '15px',
+        base: '20px',
+        md: '20px',
+        lg: '25px',
+        xl: '30px',
+        '2xl': '35px',
+        '3xl': '40px',
+        '4xl': '45px',
+        '5xl': '50px', 
+        '6xl': '55px',
+        '7xl': '60px',
       },
     },
     fontFamily: {
-      sans: ['"VT323"', 'monospace'],
+      sans: ['"Rubik"', 'serif'],
     },
   },
   plugins: [],
