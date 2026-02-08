c40de33ef3f9d498780413117f1079502920e71d
ColeChiodo <colechiodo@gmail.com>
2026-02-07 21:13:25 -0800

    read backend url from .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,32 +1,39 @@
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  css: {
-    postcss: "./postcss.config.js",
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-      "/auth": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-      "/socket.io": {
-        target: "http://localhost:3000",
-        ws: true,
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
+    css: {
+      postcss: "./postcss.config.js",
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
+        "/auth": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
+        "/socket.io": {
+          target: env.VITE_BACKEND_URL,
+          ws: true,
+        },
+      },
+    },
+  };
 });
+
